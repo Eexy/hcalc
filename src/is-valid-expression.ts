7e2c3bbc536aff9641fdd50932888f389c5a2146
eexy <eexdhe@gmail.com>
2023-06-07 09:27:26 +0200

    feat: add function to validate expression

---
+++ src/is-valid-expression.ts
@@ -0,0 +1,25 @@
+export function isValidExpression(s: string): boolean {
+  const operands = [] as string[];
+
+  let prev = '';
+  for (const c of s) {
+    if (['+', '-', '*', '/'].includes(c)) {
+      if (prev.length) {
+        operands.push(prev);
+        prev = '';
+      }
+
+      const top = operands.pop();
+
+      if (!top) return false;
+    } else {
+      prev += c;
+    }
+  }
+
+  if (prev.length) {
+    operands.push(prev);
+  }
+
+  return operands.length === 1;
+}
