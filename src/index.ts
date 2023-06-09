2c7adf104191ace8c4b42adb7c7bbf54b5c8036a
eexy <eexdhe@gmail.com>
2023-06-09 14:45:19 +0200

    feat: add cli params to show history and to clear it

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import { buildExpressionTree } from "./build-expression-tree";
 import { buildPostfix } from "./build-postfix";
 import { calc } from "./calc";
@@ -17,8 +17,18 @@ if (params[0] && isValidExpression(params[0])) {
     saveInHistory(`${params[0]} = ${val}`);
   }
 } else if (params[0] === "-log") {
-  const content = readFileSync("./history.txt", "utf-8");
-  console.info(content);
+  if (params[1] && params[1] === "--clear") {
+    writeFileSync("./history.txt", "", "utf-8");
+  } else {
+    try {
+      const content = readFileSync("./history.txt", "utf-8");
+      console.info(content);
+    } catch {
+      console.error(
+        "Unable to get history. File doesn't exist or is corrupted"
+      );
+    }
+  }
 } else {
   console.error(
     "Invalid params passed. You can see the list of options by using '-help' or '-h'"
